32aea34a2789881a3aca37ca7558a0bd5c05c765
thinkverse <hallberg.kim@gmail.com>
2020-08-17 00:24:25 +0200

    Refactor page structure

--- src/App.js
+++ src/App.js
@@ -16,104 +16,108 @@ function App() {
   return (
     (result.loaded) ? (
     <div className="app recipe">
-      <header className="header">
-        <h1>{ result.recipe.title }</h1>
-      </header>
-      <aside className="description">
-        <span className="material-icons">
-          drag_indicator
-        </span>
-        <p>{ result.recipe.description }</p>
-      </aside>
-      <figure className="cover-image">
-        <img src="./img/photo1.png" alt="{ result.recipe.title }" />
-      </figure>
-      <section className="meta">
-        <div className="meta__item meta__item--full">
-          <i className="material-icons">local_dining</i>
-          <div className="meta__body">
-            <header>
-              Yields
-            </header>
-            <span>
-              { result.recipe.meta.yields } Servings
-            </span>
-          </div>
-        </div>
-        <div className="meta__item">
-          <i className="material-icons">schedule</i>
-          <div className="meta__body">
-            <header>
-              Prep time
-            </header>
-            <span>
-              { result.recipe.meta.prep } minutes
-            </span>
-          </div>
-        </div>
-        <div className="meta__item">
-          <i className="material-icons">schedule</i>
-          <div className="meta__body">
-            <header>
-              Cook time
-            </header>
-            <span>
-              { result.recipe.meta.cook / 60 } hour
-            </span>
-          </div>
-        </div>
-        <div className="meta__item">
-          <i className="material-icons">schedule</i>
-          <div className="meta__body">
-            <header>
-              Total time
-            </header>
-            <span>
-              { result.recipe.meta.total / 60 } hours
-            </span>
-          </div>
-        </div>
-      </section>
-      <section className="ingredients">
-        <header>
-          <h2>Ingredients</h2>
+      <main>
+        <header className="header">
+          <h1>{ result.recipe.title }</h1>
         </header>
-        <div>
-          { result.recipe.ingredients.map(part => {
-            return (
-              <>
-                <header key={part.key}>
-                  <h3>
-                    <ReactMarkdown source={part.title} />
-                  </h3>
+        <aside className="description">
+          <span className="material-icons">
+            drag_indicator
+          </span>
+          <p>{ result.recipe.description }</p>
+        </aside>
+        <figure className="cover-image">
+          <img src="./img/photo1.png" alt="{ result.recipe.title }" />
+        </figure>
+        <article>
+          <aside className="meta">
+            <div className="meta__item meta__item--full">
+              <i className="material-icons">local_dining</i>
+              <div className="meta__body">
+                <header>
+                  Yields
                 </header>
-                <section className="ingredient">
-                  { part.ingredient.map(val => {
-                    return (
-                      <label>
-                        <input type="checkbox" className="checkbox" />
-                        <span>
-                          <span>{ val.units }</span>
-                          { val.weight ? (
-                            <span> ({ val.weight }g) </span>
-                          ) : ' ' }
-                          <ReactMarkdown source={ val.name.toLowerCase() } />
+                <span>
+                  { result.recipe.meta.yields } Servings
+                </span>
+              </div>
+            </div>
+            <div className="meta__item">
+              <i className="material-icons">schedule</i>
+              <div className="meta__body">
+                <header>
+                  Prep time
+                </header>
+                <span>
+                  { result.recipe.meta.prep } minutes
+                </span>
+              </div>
+            </div>
+            <div className="meta__item">
+              <i className="material-icons">schedule</i>
+              <div className="meta__body">
+                <header>
+                  Cook time
+                </header>
+                <span>
+                  { result.recipe.meta.cook / 60 } hour
+                </span>
+              </div>
+            </div>
+            <div className="meta__item">
+              <i className="material-icons">schedule</i>
+              <div className="meta__body">
+                <header>
+                  Total time
+                </header>
+                <span>
+                  { result.recipe.meta.total / 60 } hours
+                </span>
+              </div>
+            </div>
+          </aside>
+          <section className="ingredients">
+            <header>
+              <h2>Ingredients</h2>
+            </header>
+            <div>
+              { result.recipe.ingredients.map(part => {
+                return (
+                  <>
+                    <header key={part.key}>
+                      <h3>
+                        <ReactMarkdown source={part.title} />
+                      </h3>
+                    </header>
+                    <section className="ingredient">
+                      { part.ingredient.map(val => {
+                        return (
+                          <label>
+                            <input type="checkbox" className="checkbox" />
+                            <span>
+                              <span>{ val.units }</span>
+                              { val.weight ? (
+                                <span> ({ val.weight }g) </span>
+                              ) : ' ' }
+                              <ReactMarkdown source={ val.name.toLowerCase() } />
 
-                          { val.meta?.info ? (
-                            <>&nbsp;
-                              <ReactMarkdown source={ val.meta?.info.toLowerCase() } />
-                            </>
-                          ): '' }
-                        </span>
-                      </label>
-                    )
-                  }) }
-                </section>
-              </>
-            )
-          }) }
-        </div>
-      </section>
+                              { val.meta?.info ? (
+                                <>&nbsp;
+                                  <ReactMarkdown source={ val.meta?.info.toLowerCase() } />
+                                </>
+                              ): '' }
+                            </span>
+                          </label>
+                        )
+                      }) }
+                    </section>
+                  </>
+                )
+              }) }
+            </div>
+          </section>
+        </article>
+      </main>
       <footer>
         <p>Thinkverse &copy; <a href="https://devchallenges.io">DevChallenges.io</a></p>
       </footer>
